790f5f87ea1a5d7d227ca69f47a3d98ccd0de20b
Andrew Albrecht <andrewalbrecht@utexas.edu>
2019-06-27 19:19:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+🌐 MERN Stack
+</h1>
---
+++ client/src/components/tournament.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Tournament extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Tournament List</h1>
+      <ul>
+          <li>Tourney 1</li>
+          <li>Tourney 2</li>
+      </ul>
+      </div>
+    );
+  }
+}
